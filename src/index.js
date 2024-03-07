604f6e7478b4278deb044b04a97ec6b82dbb4fc1
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-03-07 15:26:17 +0100

    Finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const timeRemai = document.getElementById("time")
 const buttonStart = document.getElementById("start-btn")
+const toastDiv = document.querySelector('.toast')
+const closeButton = document.querySelector('#close-toast')
+
 const DURATION = 3; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
-
 
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 buttonStart.addEventListener("click", startCountdown)
+
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
@@ -22,35 +24,24 @@ function startCountdown() {
     } else {
       clearInterval(timer)
       timeRemai.textContent = remainingTime;
-      // showToast()
+      showToast()
     }
   }, 1000)
-
-
-  // ? On each interval tick, the remaining time (seconds) should be decreased by 1.
-  // ? On each interval tick, the div#time element should be updated to display the remaining time.
-  // ? When the remaining time reaches 0, the countdown timer should be stopped.
-  // ? When the remaining time reaches 0, the showToast() function should be called to display a toast message to the user (you'll implement this function in the next iteration).
-
-  // ?? The start button (#start-btn) should be disabled during the countdown using the disabled property, so that the user can't click it.
-
-
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
-
-
-
+  const toastShow = toastDiv.classList
+  toastShow.add('show');
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
+  closeButton.addEventListener("click", (event) => {
+    toastShow.remove('show');
+  })
 
 }
